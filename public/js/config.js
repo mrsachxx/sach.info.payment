13bd2040e209b2095176d8a66518aac71b0360f3
mrbok <son.van@gooya.co.jp>
2022-05-10 15:41:48 +0700

    okok

---
+++ public/js/config.js
@@ -0,0 +1,48 @@
+const WEB = {
+  title: "Nguyễn Đăng Sách",
+  favicon: "dollar.png",
+};
+const USER = {
+  username: "Nguyễn Đăng Sách",
+  blueTick: true,
+  avatar: "avatar.jpeg",
+  skills: ["Sad boy"],
+  banks: [
+    {
+      name: "Binance Smart Chain",
+      logo: "bsc.png",
+      number: "0x00f1981030CcF6fADDB17dC90F6c949754B9837A",
+      owner: "BEP20"
+    },
+    {
+      name: "Momo",
+      logo: "momo.png",
+      number: "0358290623",
+      owner: "NGUYEN DANG SACH",
+      fastTransfer: true
+    },
+    // {
+    //   name: "TP Bank",
+    //   logo: "tpbank.png",
+    //   number: "02495109501",
+    //   owner: "NGUYEN DANG SACH"
+    // },
+    {
+      name: "MB Bank",
+      logo: "mbbank.png",
+      number: "000019101995",
+      owner: "NGUYEN DANG SACH"
+    },
+    {
+      name: "MB Bank",
+      logo: "mbbank.png",
+      number: "1020120848190",
+      owner: "NGUYEN DANG SACH"
+    },
+    {
+      name: "Techcombank",
+      logo: "techcombank.png",
+      number: "19035469294012",
+      owner: "NGUYEN DANG SACH"
+    }]
+};
